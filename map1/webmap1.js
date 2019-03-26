d6b61a4ea7dc1ec3eb19876df1b0cf802c355be0
chiphan225 <48706363+chiphan225@users.noreply.github.com>
2019-03-26 15:31:35 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,1 +1,5 @@
-let map1 = L.map('map1').setView([32.18, -99.14], 4)
+let map1 = L.map('map1').setView([30.4077116,-91.1818346], 4)
+var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+	maxZoom: 18,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
