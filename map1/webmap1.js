0f2b6ef72f761b80e3d7ae8ac71792962f4e99c0
chiphan225 <48706363+chiphan225@users.noreply.github.com>
2019-03-26 15:35:53 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,5 +1,16 @@
 let map1 = L.map('map1').setView([30.4077116,-91.1818346], 4)
+
 var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
+let pft = L.marker([30.4077116,-91.1818346]).addTo(map1)
+
+let patrickftaylorhall = L.polygon([
+  [51.509, -0.08],
+  [51.503, -0.06],
+  [51.51, -0.047]
+]).addTo(map1);
+
+pft.bindPopup('A polygon')
+patrickftaylorhall.bindPopup('A marker')
