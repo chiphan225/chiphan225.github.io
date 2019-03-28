2268ed152652c7e8ff46d23ba9d0174b21d0092c
chiphan225 <48706363+chiphan225@users.noreply.github.com>
2019-03-28 11:14:26 -0500

    Update webmap2.js

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,4 +1,5 @@
 let map2 = L.map('map2').setView([30.4077116,-91.1818346], 4)
+L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(map2)
 var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
