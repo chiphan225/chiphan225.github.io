878ef5299250ffbc505f72cd0687e8645f1171ae
chiphan225 <48706363+chiphan225@users.noreply.github.com>
2019-03-28 11:52:08 -0500

    Add files via upload

--- map2/webmap2.js
+++ map2/webmap2.js
@@ -1,18 +1,21 @@
-let weathermap = L.map('map2').setView([30.4077116,-91.1818346], 4)
+let map2 = L.map('map2').setView([30.4077116,-91.1818346], 4)
 L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png').addTo(map2)
 var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
 	maxZoom: 17,
 	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
-var L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
+L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
   layers: 'nexrad-n0r-900913',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
 }).addTo(map2)
-var L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
+L.tileLayer.wms('https://mesonet.agron.iastate.edu/cgi-bin/wms/us/wwa.cgi', {
   layers: 'warnings_c',
   format: 'image/png',
   transparent: true,
   attribution: 'NOAA, Iowa State University'
+}).addTo(map2)
+L.esri.dynamicMapLayer({
+  url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
 }).addTo(map2)
