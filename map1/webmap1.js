b4da83e9c20690f92bbb808a1ed31fdff8d99d60
chiphan225 <48706363+chiphan225@users.noreply.github.com>
2019-03-28 10:33:16 -0500

    Update webmap1.js

--- map1/webmap1.js
+++ map1/webmap1.js
@@ -1,8 +1,8 @@
 let map1 = L.map('map1').setView([30.4077116,-91.1818346], 4)
 
-var OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
-	maxZoom: 18,
-	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+	maxZoom: 17,
+	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
 });
 let pft = L.marker([30.4077116,-91.1818346]).addTo(map1)
 
