58159f7b0b9b1bba454e923ee1cf80b067909b51
chiphan225 <48706363+chiphan225@users.noreply.github.com>
2019-04-09 22:05:43 -0500

    Update webmap3.js

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -8,10 +8,6 @@ let stateDemographicsUrl = 'https://geog4046.github.io/portfolio/data/us_state_d
 jQuery.getJSON(stateDemographicsUrl, function (data) {
   L.geoJSON(data).addTo(stateMap)
 })
-let stateStyle = { color: 'green' }
-let stateGeojsonOptions = { style: stateStyle }
-L.geoJSON(data, stateGeojsonOptions).addTo(stateMap)
-
 let stateStyle = function (feature) {
   let age = feature.properties.MED_AGE // get the current state's Median Age attribute
   let stateColor = 'olive' // let the initial color be a darker green
